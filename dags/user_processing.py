04f13a7d42fc6a455bdf6fa3795e5df9254bd914
Pankaj Kushwaha <pankaj.kushwaha@yahoo.com>
2022-05-21 19:08:36 +0000

    update file

--- dags/user_processing.py
+++ dags/user_processing.py
@@ -9,8 +9,7 @@ from datetime import datetime
 default_args = {'start_date': datetime(2022, 1, 1)}
 
 
-with DAG('user_processing', schedule_interval='@daily', 
-  default_args=default_args, catchup=False) as dag
+with DAG('user_processing', schedule_interval='@daily', default_args=default_args, catchup=False) as dag:
 
 #Define tasks/operators
   creating_table = SqliteOperator(
