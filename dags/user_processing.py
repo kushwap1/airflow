fdaa4cc245dd6950cb32cc6dfbe924def2124699
Pankaj Kushwaha <pankaj.kushwaha@yahoo.com>
2022-05-21 19:48:17 +0000

    update file

--- dags/user_processing.py
+++ dags/user_processing.py
@@ -6,7 +6,11 @@ from airflow.providers.sqlite.operators.sqlite import SqliteOperator
 
 from airflow.providers.http.sensors.http import HttpSensor
 
+from airflow.providers.http.operators.http import SimpleHttpOperator
+
 from datetime import datetime
+
+import json
 
 default_args = {'start_date': datetime(2022, 1, 1)}
 
@@ -34,4 +38,13 @@ with DAG('user_processing', schedule_interval='@daily', default_args=default_arg
       task_id = 'is_api_available',
       http_conn_id = 'user_api',
       endpoint = 'api/'
+  )
+
+  extracting_user = SimpleHttpOperator(
+      task_id = 'extracting_user',
+      http_conn_id = 'user_api',
+      endpoint = 'api/'
+      method = 'GET'
+      response_filter = lambda response: json.loads(response.text),
+      log_response = True
   )
