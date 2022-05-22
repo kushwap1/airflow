746124f4793a674e1e05120c12366b01fc69adec
Pankaj Kushwaha <pankaj.kushwaha@yahoo.com>
2022-05-22 18:55:20 +0000

    wd

--- dags/parallel_dag.py
+++ dags/parallel_dag.py
@@ -13,17 +13,17 @@ with DAG('parallel_dag', schedule_interval='@daily', default_args=default_args, 
     bash_command = 'sleep 3'
   )
 
-    task_2 = BashOperator(
+  task_2 = BashOperator(
     task_id = 'task_2',
     bash_command = 'sleep 3'
   )
 
-    task_3 = BashOperator(
+  task_3 = BashOperator(
     task_id = 'task_3',
     bash_command = 'sleep 3'
   )
 
-    task_4 = BashOperator(
+  task_4 = BashOperator(
     task_id = 'task_4',
     bash_command = 'sleep 3'
   )
