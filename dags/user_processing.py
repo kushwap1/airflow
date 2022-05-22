c64ceb21462e5cd16686e926299b1508a25f8b8d
Pankaj Kushwaha <pankaj.kushwaha@yahoo.com>
2022-05-22 18:18:43 +0000

    wd

--- dags/user_processing.py
+++ dags/user_processing.py
@@ -41,7 +41,7 @@ with DAG('user_processing', schedule_interval='@daily', default_args=default_arg
     task_id = 'creating_table',
     sqlite_conn_id = 'db_sqlite',
     sql = '''
-      CREATE TABLE users (
+      CREATE TABLE IF NOT EXISTS users (
           firstname TEXT NOT NULL,
           lastname TEXT NOT NULL,
           country TEXT NOT NULL,
